3f909f82b28b71452c274c38c29b3319f3d0652f
taylan89 <taylan@eniture-dev.com>
2021-03-15 09:49:55 +0500

    Fixes bug related to order widget details

--- Block/Adminhtml/Order/View/Tab/OrderDetailWidget.php
+++ Block/Adminhtml/Order/View/Tab/OrderDetailWidget.php
@@ -116,6 +116,6 @@ class OrderDetailWidget extends Template implements TabInterface
      */
     public function getTabUrl()
     {
-        return $this->getUrl('orderdetailwidget/*/OrderDetailWidget', ['_current' => true]);
+        return $this->getUrl('orderdetailwidget_wweltl/*/OrderDetailWidget', ['_current' => true]);
     }
 }
